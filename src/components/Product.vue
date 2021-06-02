bd5e2ae477c4f92718509d64d3f6e0942f2df521
Jose Arniel Pama <pamajosearniel@gmail.com>
2021-06-03 06:33:54 +0800

    Fix z-index for trash icon

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -2,7 +2,7 @@
 <div class="overflow-hidden rounded-2xl h-90 lg:w-1/4 md:w-1/2 p-4 w-full product-card scale-on-hover">
     <div class="relative">
       <img loading="lazy" :alt="product.name" :src="product.image_src" class="rounded-t-lg object-cover object-center w-full h-full block"/>
-      <button @click="$emit('delete-product')" class="modal-close absolute top-0 right-0 align-middle cursor-pointer flex flex-col items-center mt-4 mr-4 text-white z-50 rounded-full bg-black opacity-50 p-2 hidden">
+      <button @click="$emit('delete-product')" class="modal-close absolute top-0 right-0 align-middle cursor-pointer flex flex-col items-center mt-4 mr-4 text-white rounded-full bg-black opacity-50 p-2 hidden">
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
           <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
           <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
