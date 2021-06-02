846cc1a15751023d1e4f780f87f3d6fe61dca558
jose.nuvensis <arniel.pama@nuvensis.ch>
2021-06-02 10:29:56 +0800

    Layout search bar and products grid

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,24 @@
 <template>
-<section id="app" class="text-gray-600 body-font">
-  <div class="container px-5 py-24 mx-auto">
+<section id="app" class="text-gray-600 body-font bg-gray-200">
+  
+  <div class="max-w-md mx-auto rounded-lg overflow-hidden md:max-w-xl">
+    <div class="md:flex">
+      <div class="w-full p-3 mt-8">
+          <div class="relative">
+            <input type="text" class="bg-white h-14 w-full px-6 rounded-lg focus:outline-none hover:cursor-pointer" placeholder="Search anything...">
+            <button class="outline-none focus:outline-none absolute top-4 right-5 border-l pl-4">
+              <svg class="w-5 text-gray-600 h-5 cursor-pointer" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" stroke="currentColor" viewBox="0 0 24 24">
+                <path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
+              </svg>
+            </button>
+            
+          </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- the product grids -->
+  <div class="container px-5 sm:px-10 pt-14 pb-20 mx-auto">
     <div class="flex flex-wrap -m-4">
       <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
         <a class="block relative h-48 rounded overflow-hidden">
@@ -97,12 +115,5 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
